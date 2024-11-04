a16fab1385bf1a0c35ed15aea7b0a9368d6417f7
Michal Stolarczyk <michal.stolarczyk@workday.com>
2024-11-04 12:03:19 -0700

    updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,18 +17,35 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+
+      opts = {
+        colorscheme = "gotham256",
+      },
+    },
+
+    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+    -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+
+    -- use mini.starter instead of alpha
+    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+
+    -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
+    { import = "lazyvim.plugins.extras.lang.json" },
+
+    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
@@ -40,9 +57,7 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "nvimtree",
         "tarPlugin",
         "tohtml",
         "tutor",
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,80 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local uv = vim.loop
+local path_sep = uv.os_uname().version:match("Windows") and "\\" or "/"
+
+---Join path segments that were passed as input
+---@return string
+function _G.join_paths(...)
+  local result = table.concat({ ... }, path_sep)
+  return result
+end
+
+function _G.get_cache_dir()
+  local lvim_cache_dir = os.getenv("NVIM_CACHE_DIR")
+  if not lvim_cache_dir then
+    return vim.call("stdpath", "cache")
+  end
+  return lvim_cache_dir
+end
+
+local undodir = join_paths(get_cache_dir(), "undo")
+
+-- misc settings
+local default_options = {
+  backup = false, -- creates a backup file
+  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+  cmdheight = 1, -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0, -- so that `` is visible in markdown files
+  fileencoding = "utf-8", -- the encoding written to a file
+  foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
+  foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+  hidden = true, -- required to keep multiple buffers and open multiple buffers
+  hlsearch = true, -- highlight all matches on previous search pattern
+  ignorecase = true, -- ignore case in search patterns
+  mouse = "a", -- allow the mouse to be used in neovim
+  pumheight = 10, -- pop up menu height
+  showmode = false, -- we don't need to see things like -- INSERT -- anymore
+  smartcase = true, -- smart case
+  splitbelow = true, -- force all horizontal splits to go below current window
+  splitright = true, -- force all vertical splits to go to the right of current window
+  swapfile = false, -- creates a swapfile
+  termguicolors = true, -- set term gui colors (most terminals support this)
+  timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
+  title = true, -- set the title of window to the value of the titlestring
+  -- opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
+  undodir = undodir, -- set an undo directory
+  undofile = true, -- enable persistent undo
+  updatetime = 100, -- faster completion
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true, -- convert tabs to spaces
+  shiftwidth = 2, -- the number of spaces inserted for each indentation
+  tabstop = 2, -- insert 2 spaces for a tab
+  cursorline = true, -- highlight the current line
+  number = true, -- set numbered lines
+  numberwidth = 4, -- set number column width to 2 {default 4}
+  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  wrap = false, -- display lines as one long line
+  shadafile = join_paths(get_cache_dir(), "mynvim.shada"),
+  scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
+  sidescrolloff = 8, -- minimal number of screen lines to keep left and right of the cursor.
+  showcmd = false,
+  ruler = false,
+  laststatus = 3,
+}
+
+for k, v in pairs(default_options) do
+  vim.opt[k] = v
+end
+
+---  SETTINGS  ---
+vim.opt.spelllang:append("cjk") -- disable spellchecking for asian characters (VIM algorithm does not support it)
+vim.opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
+vim.opt.shortmess:append("I") -- don't show the default intro message
+vim.opt.whichwrap:append("<,>,[,],h,l")
+
+vim.opt.more = false -- don't pause listing when screen is filled
+vim.opt.swapfile = false -- don't use a swap file
