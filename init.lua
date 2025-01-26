810adf6a031975d8a7300f1f5f7373f49f142c4c
Michal Stolarczyk <michal.stolarczyk@workday.com>
2025-01-25 22:15:09 -0700

    updates

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ if vim.g.neovide then
   vim.opt.guifont = { "Berkeley Mono", ":h18" }
 end
 
+-- pipe mostly used for external integrations with the editor (from within Godot, etc)
 local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
 if not vim.loop.fs_stat(pipepath) then
   vim.fn.serverstart(pipepath)
